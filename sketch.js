4c691cefe2ec4931fe83d16dc427e75100368f30
Mormoreno <32307671+Mormoreno@users.noreply.github.com>
2017-10-09 18:13:45 +0200

    Version 2.1 flower

--- sketch.js
+++ sketch.js
@@ -5,19 +5,59 @@ function setup() {
   background(0,0,0);
 }
 
-var raggio=200;
+var raggio=100;
+var offset=-100;
 var angolo=0;
+var angolo2=0;
+var angolo3=0;
+var angolo4=0;
+var velocita=5;
 
 function draw() {
 
-	angolo=frameCount*3;
-	
-	if(angolo>=360)
-	noLoop();
-	
 	
 	stroke(255,255,255);
-  //line(width/2+raggio,height/2,width/2-raggio,height/2);
-  
-  line(width/2+raggio,height/2,width/2-(raggio*cos(angolo)),height/2+(raggio*sin(angolo)));
+
+
+  if(angolo<180)
+  {
+    angolo=-180+frameCount*velocita;
+    //Primo cerchio
+     line(offset+width/2+raggio,height/2,offset+width/2-(raggio*cos(angolo)),height/2+(raggio*sin(angolo)));
+    
+  }
+  else
+  {
+    if(angolo2<360)
+    {
+    angolo2=-360+frameCount*velocita;
+    //Secondo cerchio
+    line(offset+width/2+raggio,height/2,-offset+width/2-(raggio*cos(angolo2)),height/2-(raggio*sin(angolo2)));
+    }
+   else
+      {
+        if(angolo3<540)
+        {
+         angolo3=-540+frameCount*velocita;
+         //Terzo cerchio
+         line(offset+width/2+raggio,height/2,width/2+(raggio*sin(angolo3)),offset+height/2-(raggio*cos(angolo3)));
+        }
+         else
+          {
+            if(angolo4<720)
+            {
+             angolo4=-720+frameCount*velocita;
+             //Terzo cerchio
+             line(offset+width/2+raggio,height/2,width/2-(raggio*sin(angolo4)),-offset+height/2-(raggio*cos(angolo4)));
+            }
+            else
+            {
+              
+              
+            noLoop();              
+            }
+         }
+      }
+    
+  }
 }
